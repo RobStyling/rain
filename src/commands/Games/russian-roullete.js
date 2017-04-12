891cbe14be01da8fa4044fe4d139bd8aad064c2f
Robin <moelrobi@gmail.com>
2017-04-12 14:42:44 +0200

    should work like that. never tested tho.

--- src/commands/Games/russian-roullete.js
+++ src/commands/Games/russian-roullete.js
@@ -2,34 +2,50 @@ const sql = require("sqlite");
 sql.open("./db/scores.db");
 
 exports.run = async(client, msg) => {
-    const row = await sql.get(`SELECT * FROM scores WHERE userId ='${msg.author.id}'`)
-    if (!row) return msg.reply("Well, you dont have any Points to play...");
-    if (row.points < 120) return msg.reply("You dont have enough points to play.");
-    await msg.channel.sendMessage("A New Game of Russian Roullete has been opened!\nWho also wants to feel the deadly fear of the Gun. Join the Game for 120 Points!\nIf you wanna Join type: ```join```")
-    const responsesRaw = await msg.channel.awaitMessages(response => response.content === "join", {
-        max: 4,
-        time: 20000,
-    })
-    if (responsesRaw.size < 1) return msg.reply("well no wants to die... Aborting..");
-    const responses = Array.from(responsesRaw.values());
-    const playerone = msg.author;
-    const playertwo = responses[1].author;
-    const playerthree = responses[2].author;
-    const playerfour = responses[3].author;
-    const playerfive = responses[4].author; 
-    const rowplayertwo = await sql.get(`SELECT * FROM scores WHERE userId ='${playertwo}'`)
-    const rowplayerthree = await sql.get(`SELECT * FROM scores WHERE userId ='${playerthree}'`)
-    const rowplayerfour = await sql.get(`SELECT * FROM scores WHERE userId ='${playerfour}'`)
-    const rowplayerfive = await sql.get(`SELECT * FROM scores WHERE userId ='${playerfive}'`)
-    if (!rowplayertwo) return msg.reply("Well, you dont have any Points to play..." + playertwo);
-    if (rowplayertwo.points < 120) return msg.reply("You dont have enough points to play, "+ playertwo);
-    if (!rowplayerthree) return msg.reply("Well, you dont have any Points to play..." + playerthree);
-    if (rowplayerthree.points < 120) return msg.reply("You dont have enough points to play, "+ playerthree);
-    if (!rowplayerfour) return msg.reply("Well, you dont have any Points to play..." + playerfour);
-    if (rowplayerfour.points < 120) return msg.reply("You dont have enough points to play, "+ playerfour);
-    if (!rowplayerfive) return msg.reply("Well, you dont have any Points to play..." + playerfive);
-    if (rowplayerfive.points < 120) return msg.reply("You dont have enough points to play, "+ playerfive);
-    msg.reply("DEBUG MESSAGE:\n"+playerone + " can play with " + row.points + "\n"+playertwo+" can play with " + rowplayertwo.points + "\n" + playerthree + " can play with " + rowplayerthree.points + "\n" + playerfour + " can play with " + rowplayerfour.points + "\n" + playerfive + " can play with " + rowplayerfive.points);
+    var playerone;
+    var playertwo;
+    var playerthree;
+    await msg.channel.sendMessage("A New Game of Russian Roullete has been opened!\nWho also wants to feel the deadly fear of the Gun. Join the Game for 120 Pointsn!\nIf you wanna Join type: ```join```");
+    await msg.reply("yes you also need to join!");
+    const collector = msg.channel.createCollector(m => m.content.startsWith('join'), {
+        maxMatches: 3,
+        time: 20000
+    });
+    collector.on('message', async(msg, collected) => {
+        if (playerone == undefined) {
+            const playeronerow = await sql.get(`SELECT * FROM scores WHERE userId ='${msg.author.id}'`);
+            if (!playeronerow) return msg.reply("Well, you dont have any Points to play...");
+            if (playeronerow.points < 120) return msg.reply("You dont have enough points to play.");
+            playerone = msg.author;
+            return
+        }
+        if (playertwo == undefined) {
+            if (playerone != msg.author) {
+                const playertworow = await sql.get(`SELECT * FROM scores WHERE userId ='${msg.author.id}'`);
+                if (!playertworow) return msg.reply("Well, you dont have any Points to play...");
+                if (playertworow.points < 120) return msg.reply("You dont have enough points to play.");
+                playertwo = msg.author;
+                return
+            }
+        }
+        if (playerthree == undefined) {
+            if (playerone != msg.author && playertwo != msg.author) {
+                const playerthreerow = await sql.get(`SELECT * FROM scores WHERE userId ='${msg.author.id}'`);
+                if (!playerthreerow) return msg.reply("Well, you dont have any Points to play...");
+                if (playerthreerow.points < 120) return msg.reply("You dont have enough points to play.");
+                playerthree = msg.author;
+                return
+            }
+        }
+    });
+
+    collector.on('end', collected => {
+        if(playertwo == undefined && playerthree == undefined) return msg.channel.sendMessage("no want to die.. sad.");
+        var players = [playerone, playertwo];
+        if(playerthree != undefined) { players.push(playerthree);}
+        var winner = players[Math.floor(Math.random()*players.length)];
+        msg.reply("AND THE WINNER IS: " + winner);
+        });
 }
 
 exports.conf = {
